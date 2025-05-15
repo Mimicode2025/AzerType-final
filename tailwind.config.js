8b78dc5c11366596bce6d06384bb26ce753e6d09
Mimicode2025 <traoredermanemukara@gmail.com>
2025-05-15 20:21:57 +0000

    Configuration initiale de TogoLang

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 export default {
   content: [
     "./index.html",
-    "./js/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+        }
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
